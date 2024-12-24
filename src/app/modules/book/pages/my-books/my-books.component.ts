f9d63981a1b46ba579eebdc06918aae4d810253e
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2024-12-24 15:33:13 -0500

    feat: my books

--- src/app/modules/book/pages/my-books/my-books.component.ts
+++ src/app/modules/book/pages/my-books/my-books.component.ts
@@ -1,12 +1,80 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import {
+  PageResponseBookResponse,
+  BookResponse,
+} from '../../../../services/models';
+import { BookService } from '../../../../services/services';
+import { FormsModule } from '@angular/forms';
+import { BookCardComponent } from '../../components/book-card/book-card.component';
 
 @Component({
   selector: 'app-my-books',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, BookCardComponent],
   templateUrl: './my-books.component.html',
   styleUrls: ['./my-books.component.scss'],
 })
-export class MyBooksComponent {}
+export class MyBooksComponent implements OnInit {
+  bookResponse: PageResponseBookResponse = {};
+  page: number = 0;
+  size: number = 3;
+
+  constructor(private bookService: BookService, private router: Router) {}
+
+  ngOnInit() {
+    this.findAllBooks();
+  }
+
+  private findAllBooks() {
+    this.bookService
+      .findAllBooksByOwner({
+        page: this.page,
+        size: this.size,
+      })
+      .subscribe({
+        next: (books: PageResponseBookResponse) => {
+          this.bookResponse = books;
+          console.log(this.bookResponse);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+
+  goToLastPage() {
+    this.page = (this.bookResponse.totalPages as number) - 1;
+    this.findAllBooks();
+  }
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+  goToPage(pageIndex: number) {
+    this.page = pageIndex;
+    this.findAllBooks();
+  }
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
+  get isLastPage(): boolean {
+    return this.page === (this.bookResponse.totalPages as number) - 1;
+  }
+
+  editBook(book: BookResponse) {
+    throw new Error('Method not implemented.');
+  }
+  shareBook(book: BookResponse) {
+    throw new Error('Method not implemented.');
+  }
+  archiveBook(book: BookResponse) {
+    throw new Error('Method not implemented.');
+  }
+}
